48f836fc2bc84188fff8eb773a66c3bad8a4f6aa
trmoro <tristan.moro@outlook.fr>
2024-10-07 16:48:21 +0200

    acmg critria put into an array

--- app.py
+++ app.py
@@ -30,6 +30,78 @@ def row2acmg(title, row):
 		var_type = "gain"
 		
 	score = float(row["Total score"])
+	acmg_criteria = []
+	if float(row["1A-B"]) != 0:
+		acmg_criteria.append("1AB " + float(row["1A-B"]))
+	if float(row["2A"]) != 0:
+		acmg_criteria.append("2A " + float(row["2A"]))
+	if float(row["2B"]) != 0:
+		acmg_criteria.append("2B " + float(row["2B"]))
+	if float(row["2C"]) != 0:
+		acmg_criteria.append("2C " + float(row["2C"]))
+	if float(row["2D"]) != 0:
+		acmg_criteria.append("2D " + float(row["2D"]))
+	if float(row["2E"]) != 0:
+		acmg_criteria.append("2E " + float(row["2E"]))
+	if float(row["2F"]) != 0:
+		acmg_criteria.append("2F " + float(row["2F"]))
+	if float(row["2G"]) != 0:
+		acmg_criteria.append("2G " + float(row["2G"]))
+	if float(row["2H"]) != 0:
+		acmg_criteria.append("2H " + float(row["2H"]))
+	if float(row["2I"]) != 0:
+		acmg_criteria.append("2I " + float(row["2I"]))
+	if float(row["2J"]) != 0:
+		acmg_criteria.append("2J " + float(row["2J"]))
+	if float(row["2K"]) != 0:
+		acmg_criteria.append("2K " + float(row["2K"]))
+	if float(row["2L"]) != 0:
+		acmg_criteria.append("2L " + float(row["2L"]))
+	if float(row["3"]) != 0:
+		acmg_criteria.append("3 " + float(row["3"]))
+	if float(row["4A"]) != 0:
+		acmg_criteria.append("4A " + float(row["4A"]))
+	if float(row["4B"]) != 0:
+		acmg_criteria.append("42 " + float(row["4B"]))
+	if float(row["4C"]) != 0:
+		acmg_criteria.append("4C " + float(row["4C"]))
+	if float(row["4D"]) != 0:
+		acmg_criteria.append("4D " + float(row["4D"]))
+	if float(row["4E"]) != 0:
+		acmg_criteria.append("4E " + float(row["4E"]))
+	if float(row["4F-H"]) != 0:
+		acmg_criteria.append("4FH " + float(row["4F-H"]))
+	if float(row["4I"]) != 0:
+		acmg_criteria.append("4I " + float(row["4I"]))
+	if float(row["4J"]) != 0:
+		acmg_criteria.append("4J " + float(row["4J"]))
+	if float(row["4K"]) != 0:
+		acmg_criteria.append("4K " + float(row["4K"]))
+	if float(row["4L"]) != 0:
+		acmg_criteria.append("4L " + float(row["4L"]))
+	if float(row["4M"]) != 0:
+		acmg_criteria.append("4M " + float(row["4M"]))
+	if float(row["4N"]) != 0:
+		acmg_criteria.append("4N " + float(row["4N"]))
+	if float(row["4O"]) != 0:
+		acmg_criteria.append("4O " + float(row["4O"]))
+	if float(row["5A"]) != 0:
+		acmg_criteria.append("5A " + float(row["5A"]))
+	if float(row["5B"]) != 0:
+		acmg_criteria.append("5B " + float(row["5B"]))
+	if float(row["5C"]) != 0:
+		acmg_criteria.append("5C " + float(row["5C"]))
+	if float(row["5D"]) != 0:
+		acmg_criteria.append("5D " + float(row["5D"]))
+	if float(row["5E"]) != 0:
+		acmg_criteria.append("5E " + float(row["5E"]))
+	if float(row["5F"]) != 0:
+		acmg_criteria.append("5F " + float(row["5F"]))
+	if float(row["5G"]) != 0:
+		acmg_criteria.append("5G " + float(row["5G"]))
+	if float(row["5H"]) != 0:
+		acmg_criteria.append("5H " + float(row["5H"]))
+
 
 	return {"title": title ,"chr":row["Chromosome"],"start":int(row["Start"]),"end":int(row["End"]),"var_type":var_type,
 	"classification": row["Classification"], "score": score,
@@ -43,7 +115,8 @@ def row2acmg(title, row):
 	"crt_4K": float(row["4K"]),"crt_4L": float(row["4L"]),"crt_4M": float(row["4M"]),"crt_4N": float(row["4N"]),
 	"crt_4O": float(row["4O"]),
 	"crt_5A": float(row["5A"]),"crt_5B": float(row["5B"]),"crt_5C": float(row["5C"]),"crt_5D": float(row["5D"]),
-	"crt_5E": float(row["5E"]),"crt_5F": float(row["5F"]),"crt_5G": float(row["5G"]),"crt_5H": float(row["5H"])
+	"crt_5E": float(row["5E"]),"crt_5F": float(row["5F"]),"crt_5G": float(row["5G"]),"crt_5H": float(row["5H"]),
+	"acmg_criteria" : acmg_criteria
 	}
 
 #Compute ACMG
